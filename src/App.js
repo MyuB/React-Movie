fc1c88142c03dea12d25a54ca84a4e7eb3bc539c
MyuB <mjycoco0@naver.com>
2022-07-26 18:54:30 +0900

    basic components created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
-import Button from "./Button";
+import PropTypes from "prop-types";
+import axios from "axios";
+import React from "react";
+import Movie from "./Movie";
 
-function App() {
-  return (
-    <div>
-      <h1>hi</h1>
-      <Button text="temp" />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+  };
+
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader_text">"Loading at the moment......"</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie) => (
+              <Movie
+                key={`${movie.title}-${movie.id}`}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
+  }
 }
 
 export default App;
